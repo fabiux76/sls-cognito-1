c9c2d4f04beea630eda16f9ca7a37d3448e7c6c6
Fabio Adani <fabio.adani@gmail.com>
2022-04-07 18:22:21 +0200

    Single commit

---
+++ aws-react-basic-app/src/config.js
@@ -0,0 +1,12 @@
+export default {
+	apiGateway: {
+		REGION: 'us-east-1',
+		URL: 'https://twv3xlvq4b.execute-api.us-east-1.amazonaws.com/dev'
+	},
+	cognito: {
+		REGION: 'us-east-1',
+		USER_POOL_ID: 'us-east-1_hlgNGcIS7',
+		APP_CLIENT_ID: '5i9htq9ejfe2d9qkhk2ec9ohkt',
+		IDENTITY_POOL_ID: 'us-east-1:dbae72ce-42b9-4cc0-8e8d-99a40ed21cab'
+	}
+};
